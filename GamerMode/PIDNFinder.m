108cdf9b17accdf912648118d593b5ddd8484151
twxed <f.w.nijkamp@student.tue.nl>
2022-05-24 16:46:29 +0200

    Im done

--- GamerMode/PIDNFinder.m
+++ GamerMode/PIDNFinder.m
@@ -4,7 +4,7 @@ TargetH_mm = 250;
 
 opts =optimoptions('lsqnonlin','display','iter-detailed','UseParallel',true,'FiniteDifferenceStepSize',5e-5);
 %Setting bounds and intial conditions
-ub = [0.1/250,0.1/2500,0.1/25];
+ub = [0.1/250,0.1/2500,0];
 lb = [0,0,0];
 init = ub/2;
 %Running the built in optimization tool.
